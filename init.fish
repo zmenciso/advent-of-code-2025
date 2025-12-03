902dd7070d84a333ed60a3dbf9787fb7f8928a1e
Zephan Enciso <zmenciso@pm.me>
2025-12-02 20:58:53 -0500

    Init: Use error crate, fix default `main` return

--- init.fish
+++ init.fish
@@ -1,11 +1,14 @@
 #!/usr/bin/fish
 
 set template "use std::io;
+use std::error::Error;
 
-fn main() -> Result<(), Box<dyn std::error::Error>>
+fn main() -> Result<(), Box<dyn Error>> {
     for line in io::stdin().lines() {
         let line = line.expect(\"Could not read line\");
     }
+
+    Ok(())
 }"
 
 set dir "aoc-$argv[1]"
@@ -13,4 +16,5 @@ mkdir $dir
 cd $dir
 
 cargo init
-echo $template > src/main.rs
+echo $template >src/main.rs
+touch src/lib.rs
